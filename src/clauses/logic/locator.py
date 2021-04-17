af0e781540cfce53c7f622c0b30c7cfe419dcbea
Jeremiah Boby <mail@jeremiahboby.me>
2021-04-17 14:00:26 +0100

    Separate clause locator logic into clauses.logic

---
+++ src/clauses/logic/locator.py
@@ -0,0 +1,93 @@
+"""Utility methods for use in the 'clauses' app."""
+
+import itertools, string
+from dataclasses import InitVar, dataclass, field
+from typing import Any, Final, Generator, Optional
+
+from clauses.hints import IndexedChar
+from clauses.logic.utils import sentence_matches_clause, strip_punctuation
+
+
+WORD_CHARS: Final[str] = string.ascii_lowercase + string.digits
+
+
+@dataclass
+class ClauseLocator:
+    """Stores and locates clauses within relevant context."""
+
+    clause_str: InitVar[str]
+    context_str: InitVar[str]
+
+    clause: list["Word"] = field(default_factory=list)
+    context: list["Word"] = field(default_factory=list)
+
+    def __post_init__(self, clause_str: str, context_str: str) -> None:
+        """Split the provided clause and context into Words."""
+        self.clause.extend(Word.from_string(clause_str))
+        self.context.extend(Word.from_string(context_str))
+
+    def locate(self) -> Optional[tuple[int, int]]:
+        """Locate the provided clause in the given context."""
+        sentence: list[Word] = []
+        sentences: list[list[Word]] = []
+
+        for word, target in itertools.product(self.context, self.clause):
+            if target == word:
+                sentence.append(word)
+
+                if not sentence_matches_clause(sentence, self.clause):
+                    previous_match = sentence[:-1]
+
+                    # Ensure the previous sentence was matching
+                    if sentence_matches_clause(previous_match, self.clause):
+                        sentences.append(previous_match)
+                        sentence = [sentence[-1]]
+                    else:
+                        sentence.clear()
+
+        if sentence:
+            sentences.append(sentence)
+
+        if not sentences:
+            return None
+
+        longest_match = max(sentences, key=len)
+        first, last = longest_match[0], longest_match[-1]
+
+        return first[0][0], last[-1][0]
+
+
+@dataclass
+class Word:
+    """Represents a collection of IndexedChars bounded by whitespace."""
+
+    characters: list[IndexedChar] = field(default_factory=list)
+
+    @classmethod
+    def from_string(cls, string: str) -> Generator["Word", None, None]:
+        """Given a string, yield all contained Words."""
+        word: list[IndexedChar] = []
+
+        for index, character in strip_punctuation(enumerate(string.lower())):
+            if character not in WORD_CHARS and word:
+                yield cls(word.copy())
+                word.clear()
+                continue
+            word.append((index, character))
+
+        if word:
+            yield cls(word)
+
+    def __eq__(self, other: Any) -> bool:
+        """Check that two words are equal."""
+        if type(other) is not Word:
+            return False
+
+        for self_char, other_char in zip(self.characters, other.characters):
+            if self_char[1] != other_char[1]:
+                return False
+        return True
+
+    def __getitem__(self, index: Any) -> IndexedChar:
+        """Slicing on this Word should apply to its characters."""
+        return self.characters[index]
---
+++ src/clauses/logic/utils.py
@@ -0,0 +1,31 @@
+"""Utility methods for use in the 'clauses' app."""
+
+from string import punctuation
+from typing import Generator, Iterable
+
+from clauses.logic import locator
+from clauses.hints import IndexedChar
+
+
+def strip_punctuation(
+    string: Iterable[IndexedChar],
+) -> Generator[IndexedChar, None, None]:
+    """
+    Return the provided string, stripped of any punctuation.
+
+    This needs to be applied after strings are split and indexed,
+    so we can retain the original indexes for each character.
+    """
+    for index, character in string:
+        if character not in punctuation:
+            yield (index, character)
+
+
+def sentence_matches_clause(
+    sentence: list["locator.Word"], clause: list["locator.Word"]
+) -> bool:
+    """Check if the provided list of Words matches a clause so far."""
+    for sentence_word, clause_word in zip(sentence, clause):
+        if sentence_word != clause_word:
+            return False
+    return bool(sentence) and True
