ecb708e30e7681c4f6e6abf6961e19e35fdbe555
Jeremiah Boby <mail@jeremiahboby.me>
2021-04-17 14:39:19 +0100

    Update urlpatterns to include api/ prefix

--- src/core/urls.py
+++ src/core/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("clauses/", include("clauses.urls", namespace="clauses")),
+    path("api/clauses/", include("clauses.urls", namespace="clauses")),
 ]
