0794ae0ceba952cd9a9335b3d6d7739b1acddee5
Jeremiah Boby <mail@jeremiahboby.me>
2021-04-17 14:20:53 +0100

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -9,7 +9,7 @@ do
 done
 echo "Done."
 
-python ./manage.py migrate --no-input
-python ./manage.py collectstatic --no-input
+python src/manage.py migrate --no-input
+python src/manage.py collectstatic --no-input
 
 exec "$@"
