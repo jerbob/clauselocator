9b77f0da423d2a6504ceab29f569cc370794a7cb
Jeremiah Boby <mail@jeremiahboby.me>
2021-04-17 14:01:02 +0100

    Update ClauseLocator APIView

---
+++ src/clauses/views.py
@@ -0,0 +1,30 @@
+"""API views for accessing clause-related logic."""
+
+from rest_framework import authentication, status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from clauses.forms import ClauseForm
+
+
+class LocateClauseView(APIView):
+    """View to locate clauses within the provided context."""
+
+    authentication_classes = [authentication.TokenAuthentication]
+
+    def post(self, request: Request) -> Response:
+        """Process user-provided parameters and return any relevant data."""
+        success, results = False, None
+        form = ClauseForm(request.data or None)
+
+        if form.is_valid():
+            success, results = form.process()
+        errors = form.errors.get_json_data()
+
+        status_code = status.HTTP_200_OK if success else status.HTTP_400_BAD_REQUEST
+        return Response(
+            data={"success": success, "results": results, "errors": errors},
+            status=status_code,
+        )
